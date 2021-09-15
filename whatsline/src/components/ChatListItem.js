e4fbe55590d38da2eeb78d2ef9497023d3b22fde
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-15 10:34:13 +0900

    ChatList: CSS

--- whatsline/src/components/ChatListItem.js
+++ whatsline/src/components/ChatListItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './ChatListItem.css';
+import mscat from "../images/mscat.jpg";
 
 export default ()=>{
     return(
-        <div>...</div>
+        <div className="chatListItem">
+            <img className="chatListItem--avatar" src={mscat} alt="" />
+            <div className="chatListItem--lines">
+                <div className="chatListItem--line">
+                <div className="chatListItem--name">Emilie Mimi</div>
+                <div className="chatListItem--date">14:00</div>
+                </div>
+                <div className="chatListItem--line">
+                    <div className="chatListItem--lastMsg">
+                        <p>R u Ok?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
