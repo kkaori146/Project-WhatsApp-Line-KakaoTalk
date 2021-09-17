1c8c18edfb7ceaadb663bf90033a05f721167013
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-17 14:36:17 +0900

    ChatList: Users

--- whatsline/src/App.js
+++ whatsline/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import ChatListItem from './components/ChatListItem';
 import ChatIntro from './components/ChatIntro';
+import ChatWindow from './components/ChatWindow';
 
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -12,8 +13,14 @@ import profile from "./images/profile.jpg";
 
 export default ()=>{
 
-  const [chatList, setChatList] = useState([{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]);
-  
+  const [chatList, setChatList] = useState([
+    {chatId: 1, title:'Ciclano', image: {profile}},
+    {chatId: 2, title:'Ciclano', image: {profile}},
+    {chatId: 3, title:'Ciclano', image: {profile}},
+    {chatId: 4, title:'Ciclano', image: {profile}},
+  ]);
+  const [activeChat, setActiveChat] = useState({});
+
   return (
     <div className="app-window">
       <div className="sidebar">
@@ -46,12 +53,19 @@ export default ()=>{
           {chatList.map((item, key)=>(
             <ChatListItem 
               key={key}
+              onClick={()=>setActiveChat(chatList[key])}
             />
           ))}
         </div>
 
       </div>
       <div className="contentarea">
+        {activeChat.chatId !== undefined &&
+          <ChatWindow />
+        }
+        {activeChat.chatId === undefined &&
+          <ChatIntro/>
+        }
             <ChatIntro />
       </div>
     </div>
