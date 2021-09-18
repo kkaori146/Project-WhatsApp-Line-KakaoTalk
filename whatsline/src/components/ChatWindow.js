1f24cb5d884ab7f6808faa9e58435d2e1bdc1d27
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-18 11:46:29 +0900

    chatWindow: structure and css

--- whatsline/src/components/ChatWindow.js
+++ whatsline/src/components/ChatWindow.js
@@ -5,6 +5,10 @@ import mscat from "../images/mscat.jpg";
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
 
 export default () =>{
     return(
@@ -33,7 +37,24 @@ export default () =>{
 
             </div>
             <div className="chatWindow--footer">
+                <div className="chatWindow--pre">
+                    <div className="chatWindow--btn">
+                        <InsertEmoticonIcon style={{color: '#919191'}} />
+                    </div>
+                </div>
 
+                <div className="chatWindow--inputarea">
+                    <input 
+                    className="chatWindow--input" 
+                    type="text" 
+                    placeholder="Digite uma Mensagem"
+                    />
+                </div>
+                <div className="chatWindow--pos">
+                <div className="chatWindow--btn">
+                        <SendIcon style={{color: '#919191'}} />
+                    </div>
+                </div>
             </div>
         </div>
     );
